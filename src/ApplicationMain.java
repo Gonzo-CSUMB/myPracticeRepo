94d63bb8bee8c7b5c696a8e9bcfe6d55aeec561a
GONZO-CSUMB <joshgonzales@csumb.edu>
2022-09-15 10:59:02 -0700

    added print

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,4 +1,28 @@
-
 public class ApplicationMain {
-
+	
+	public static void main(String[] args) {
+		String[] shopList = new String[] {
+				"Apples",
+				"Bananas",
+				"Corn",
+				"Dog Food",
+				"Cereal", 
+				"Bread", 
+				"Eggs",
+				"Milk"
+		};
+		
+		for (String item: shopList) {
+			System.out.println(item);
+		}	
+		
+		System.out.println("My Shopping List");
+        System.out.println();
+        
+        for (String item: shopList) {
+		System.out.println(item);
+	}	
+       }
+ 
 }
+
